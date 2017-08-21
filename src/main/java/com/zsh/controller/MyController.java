fc371784fed9082b731a7f5d5caac7ddf5966977
张树华 <zhangshuhua411@hotmail.com>
2017-08-21 20:28:15 +0800

    加入thymeleaf，@restController修改为Controller

--- src/main/java/com/zsh/controller/MyController.java
+++ src/main/java/com/zsh/controller/MyController.java
@@ -1,16 +1,11 @@
 package com.zsh.controller;
 
-import com.zsh.mapper.CompanyBasicMapper;
-import com.zsh.model.CompanyBasic;
 import com.zsh.service.CompanyBasicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by zsh7040 on 2017-8-21.
- */
-@RestController
+@Controller
 public class MyController {
 
     @Autowired
@@ -18,6 +13,7 @@ public class MyController {
 
     @RequestMapping("/")
     public String thing() {
-        return companyBasicService.selectOne().toString();
+        return "/hello";
     }
+
 }
